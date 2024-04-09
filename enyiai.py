703e75b53fd0db244a20f6df8427091172e0e42a
chanellw <chanel.white@crowley.k12.tx.us>
2024-04-09 16:15:04 -0500

    Cleaned code and fixed the name issue

--- enyiai.py
+++ enyiai.py
@@ -4,40 +4,23 @@ from functools import partial
 from PIL import Image
 
 class Page(customtkinter.CTk):
-    def __init__(self):
-        '''Creates a page object with a frame, buttons and labels'''
-        self.frame = None
-        self.button1 = None
-        self.button2 = None
-        self.label = None
-        self.entry = None
-
+    frame = None
+    
     def set_frame(self, f):
          self.frame = f
+
     def get_frame(self):
          return self.frame
-
-    def set_label(self, f):
-         self.label = f
-    def get_label(self):
+    
+    def create_label(self, text, parent):
+         self.label = customtkinter.CTkLabel(master=parent.get_frame(), text= text)
          return self.label
-
-    def set_button1(self, b):
-         self.button1 = b
-    def get_button1(self):
-         return self.button1
-    
-    def set_button2(self, f):
-         self.button2 = f
-    def get_button2(self):
-         return self.button2
-    
-    def set_entry(self, e):
-         self.entry = e
+          
     def get_entry(self):
          return self.entry
+    
     def create_entry(self, parent):
-        self.entry = customtkinter.CTkEntry(master=frames[0].get_frame(), placeholder_text="Name", text_color="white")
+        self.entry = customtkinter.CTkEntry(master=parent.get_frame(), placeholder_text="Name", text_color="white")
         return self.entry
 
     def create_button(self, t, com, parent):
@@ -53,6 +36,7 @@ class User():
           self.name = name
           User.id += 1
           self.uid = User.id
+          self.active = False
           
 
      def get_name(self) -> str:
@@ -60,7 +44,14 @@ class User():
      
      def get_id(self) -> int:
           return self.uid
+     
+     def set_active(self):
+          '''Sets whether the user is logged in or not'''
+          self.active = not self.active
 
+     def get_active(self):
+          '''Returns active status'''
+          return self.active
 
     
  
@@ -76,25 +67,27 @@ main_container.title("EnyiAi")
 frames = [Page(), Page(), Page()]
 user = []
 
-def get_user_name():
-        
-     new_u =  User(frames[0].get_entry().get())
-     name = new_u.get_name()
-     id = new_u.get_id()
-     print(name, id)
-     frames[0].get_entry().delete(0, len(name))
-     user.append(name)
-     user.append(id)
+def create_user_name():
+     '''Creates a new user'''
+     if User(frames[0].get_entry().get()) not in user:
+          new_u =  User(frames[0].get_entry().get())
+          frames[0].get_entry().delete(0, len(new_u.get_name()))
+          user.append(new_u)
+     return new_u.get_name()
+     
+
+def logout(u: User):
+     u.set_active()
+
 
 def make_frame3():
     '''Code for the third frame'''
     clear_frames()
-    frames[2].set_frame(customtkinter.CTkFrame(master=main_container, fg_color="blue")) 
-    frames[2].set_label(customtkinter.CTkLabel(master=frames[2].get_frame(), text= f"Welcome to the third page{user[0]}"))
+    n = user[0].get_name()
+    frames[2].set_frame(customtkinter.CTkFrame(master=main_container, fg_color="blue"))
     frames[2].get_frame().pack(pady = 20, padx=60, fill="both", expand=True)
-    frames[2].get_label().pack(pady=30, padx=10)
-    frames[2].set_button1(customtkinter.CTkButton(master=frames[2].get_frame(), text= "emotion", command=main))
-    frames[2].get_button1().pack(pady=12, padx=30, side=customtkinter.BOTTOM)
+    label = frames[2].create_label(f"Welcome to the third page {n}", frames[2])
+    label.pack(pady = 20, padx=60, fill="both", expand=True)
     button1 = frames[2].create_button("button 2", print("test create"), frames[2])
     button1.pack(pady=20, padx=30)
     frames[2].create_button("button 3", main, frames[2])
@@ -102,34 +95,35 @@ def make_frame3():
 
 def make_frame2():
     '''Code for the second frame'''
-    # TODO: figure out how to get the name to stay persistent across pages
-    #Use partial to pass parameters through the button
-    #make the name global/use the list/use dictionary/make a user class with active flag
-    #get_user_name()
-    n = user[0]
     clear_frames()
+    n = user[0].get_name()
+        
+    frames[1].set_frame(customtkinter.CTkFrame(master=main_container, fg_color="blue"))
+    frames[1].get_frame().pack(pady = 20, padx=60, fill="both", expand=True)
     
-    #frames[0].get_frame().destroy()      
-    frames[1].frame = customtkinter.CTkFrame(master=main_container, fg_color="blue")
-    frames[1].frame.pack(pady = 20, padx=60, fill="both", expand=True)
     
-    frames[1].label = customtkinter.CTkLabel(master=frames[1].frame, text= f"Welcome to the scenario trainer, {n}")
-    frames[1].label.pack(pady=30, padx=10)
-    frames[1].entry = customtkinter.CTkTextbox(master=frames[1].frame, height=100, width=200)
-    frames[1].entry.pack(pady=12, padx=20, side=customtkinter.BOTTOM)
-    frames[1].button1 = customtkinter.CTkButton(master=frames[1].frame, text="Login", command=make_frame3)        
-    frames[1].button1.pack(pady=12, padx=30)
+    label= frames[1].create_label(f"Welcome to the scenario trainer {n}", frames[1])
+    label.pack(pady=30, padx=10)
+    entry = frames[1].create_entry(frames[1])
+    entry.pack(pady=12, padx=20, side=customtkinter.BOTTOM)
+    button1 = frames[1].create_button("Login", make_frame3, frames[1])
+
+    button1.pack(pady=12, padx=30)
+
+def make_frame1():
+     
+
 def create_nav():
      left_side_panel = customtkinter.CTkFrame(main_container, width=50, corner_radius=8, fg_color="grey")
      left_side_panel.pack(side=tkinter.LEFT, fill=tkinter.Y, expand=False, padx=2, pady=20)
      home_button= customtkinter.CTkButton(master=left_side_panel, text="Home", command=main)
      home_button.pack(pady=12, padx=10)
      emotion_button = customtkinter.CTkButton(master=left_side_panel, text="Calibration", command=make_frame3)
-     #button2.pack(pady=12, padx=10)
+     
      emotion_button.pack(pady=12, padx=10)
      scenario_button = customtkinter.CTkButton(master=left_side_panel, text="Practice Scenarios", command=make_frame2)
      scenario_button.pack(pady=12, padx=10)
-     settings_button = customtkinter.CTkButton(master=left_side_panel, text="Settings", command= print("test"))
+     settings_button = customtkinter.CTkButton(master=left_side_panel, text="Settings", command= make_frame1)
      settings_button.pack(pady=12, padx=10)
      
 def clear_frames():
@@ -141,13 +135,17 @@ def clear_frames():
          frames[1].get_frame().destroy()
 
 def main():
+     '''Makes the main frame of the app'''
      clear_frames()     
      frames[0].set_frame(customtkinter.CTkFrame(master=main_container, fg_color="#34e5eb"))
      frames[0].get_frame().pack(pady = 20, padx=10, fill="both", expand=True)
-     frames[0].set_label(customtkinter.CTkLabel(master=frames[0].get_frame(), font= ("Roboto", 30), text= "Welcome to \nEnyiAi\nPlease enter your name"))
-     frames[0].get_label().pack(pady=30, padx=10)
-     frames[0].create_entry(frames[0])
-     get_user_name()
+     label= frames[0].create_label("Welcome to \nEnyiAi\nPlease enter your name", frames[0])
+     label.pack(pady=30, padx=10)
+     entry= frames[0].create_entry(frames[0])
+     entry.pack(pady=30, padx=10)
+     button = frames[0].create_button("Sign In", create_user_name, frames[0])
+     button.pack(pady=30, padx=10)
+     
      
 
 
